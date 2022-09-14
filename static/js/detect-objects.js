218221c0a3637b21a0afa83b4e81f4ec953187e1
Michael M <mmm111@hotmail.com>
2022-09-13 20:40:34 -0500

    changes

--- static/js/detect-objects.js
+++ static/js/detect-objects.js
@@ -148,7 +148,7 @@ function create_json_for_object_detection(preds){
         const currentObject = preds[i];
 
         // if (currentObject.classes[0].probability > 0.5) {        
-        if (currentObject.classes[0].probability > threshold) {
+        if (parseFloat(currentObject.classes[0].probability) > parseFloat(threshold)) {
 
             label = currentObject.classes[0].className
             confidence = Math.round(parseFloat(currentObject.classes[0].probability) * 100) + "%"; 
