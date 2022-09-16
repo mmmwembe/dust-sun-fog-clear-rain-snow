6f6c0541d38ef1305ae788ca8c67e4744747f1c8
Michael M <mmm111@hotmail.com>
2022-09-15 21:19:52 -0400

    changes

--- static/js/detect-objects.js
+++ static/js/detect-objects.js
@@ -169,7 +169,9 @@ function create_json_for_object_detection(preds){
             const highlighter = document.createElement("div");
             highlighter.setAttribute("class", "highlighter");
             highlighter.setAttribute("id", "bbox-highlighter");
-            highlighter.setAttribute("border","10px dashed #FF984F")
+            highlighter.style["border"] = "10px dashed #FF984F;"
+            // div.setAttribute('style', 'text-align: center');
+            // highlighter.setAttribute("border","10px dashed #FF984F")
             // highlighter.setAttribute("borderColor","red");
             // highlighter.setAttribute("borderStyle", "solid");
             // highlighter.setAttribute("borderWidth","10px");
