7a0bec520ac406898ed57f639a6010d124e32728
Michael M <mmm111@hotmail.com>
2022-09-17 11:36:17 -0400

    changes

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, session, redirect, url_for, request, j
 from flask_bootstrap import Bootstrap
 from functools import wraps
 from flask_pymongo import PyMongo
+import base64
+from PIL import Image
+from io import BytesIO
 
 
 app = Flask(__name__)
@@ -19,6 +22,34 @@ def home():
 @app.route('/labeling')
 def labeling():
   return render_template('labeling.html')
+
+
+@app.route('/saveCroppedImage', methods=['POST','GET'])
+
+def saveCroppedImage():
+
+    if request.method =='POST':
+
+        user_id = request.form['user_id']
+        image_name= request.form['image_name']
+        label_num= request.form['label_num']
+        currentFolder= request.form['current_folder']
+        file_name, extension = getImageNameAndExtension(image_name)
+
+        cropped_image_dataURL = request.form['imgBase64']
+        # print(cropped_image_dataURL)
+        # save cropped imageBase64 string as PNG image
+        cropped_image_file_path = 'static/images/' + user_id + '/cropped-labels/'+ currentFolder + '/' + file_name + '_' + extension + '_' + label_num + '.png'
+
+        img = Image.open(BytesIO(base64.decodebytes(bytes(cropped_image_dataURL, "utf-8"))))
+        img.save(cropped_image_file_path)
+        # saveImageBase42StringAsImage(cropped_image_dataURL)
+
+        # static/images/user1/canvas_jsons
+        
+        # encoded_string = base64.b64encode(cropped_image_dataURL)
+   
+    return jsonify(result = 'success')
 
 if __name__ == '__main__':
     
